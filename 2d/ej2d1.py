5f24abb199e1863cc3432be47a63682fdc176691
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-04-16 18:13:47 +0200

    Preparem enunciats

--- 2d/ej2d1.py
+++ 2d/ej2d1.py
@@ -1,27 +1,4 @@
 """
-Enunciado:
-Implementa una función 'convert_kg_to_lb' que reciba como parámetro un valor
-numérico llamado 'kg' que corresponde al valor que se desea convertir de 
-kilogramos a libras.
-
-El valor introducido no puede ser menor o igual que '0', si se introduce un
-valor menor o igual a '0' se debe crear un ValueError. El valor introducido
-debe ser de type numérico de manera que si se introduce otro valor que no sea
-numérico se deberá crear un TypeError.
-
-Parámetros:
-kg = Valor numérico que representa a los kilogramos para convertir a libras.
-
-Ejemplo:
-    Entrada: 50
-    Salida: 110.23
-
-    Entrada: 0
-    Salida: ValueError
-
-    Entrada: 'abc'
-    Salida: TypeError
-
 Enunciat:
 Implementa una funció 'convert_kg_to_lb' que rebi com a paràmetre un valor
 numèric anomenat 'kg' que correspon al valor que es vol convertir de
@@ -53,6 +30,5 @@ def kg_to_lb(kg):
     pass
 
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 #print(kg_to_lb(50))
--- 2d/ej2d2.py
+++ 2d/ej2d2.py
@@ -1,36 +1,4 @@
 """
-Enunciado:
-Implementa la función 'calculate_max_and_min' que reciba como parámetro una
-lista de números 'list_numbers', se deben considerar los casos en los que los
-datos que se encuentran dentro de la lista sean de type string o que la lista
-se encuentre vacía. 
-
-Adicionalmente, la función debe ir imprimiendo cual es el número menor y el
-número mayor según va avanzando en la lista siempre y cuando este sea distinto
-al anterior resultado simulando la depuración por trazas.
-
-Y finalmente, retornar un valor del número menor y del número mayor.
-
-Parámetros:
-list_numbers: Lista de números.
-
-Ejemplo:
-    Entrada: 
-        [10, 5.1, 0, -2, 31, 55, 70, -10, 200, -55.55]
-    Salida:
-        'Greater:' 200
-        'Lesser: ' -55.55
-
-    Entrada:
-        ['Hello', 1, 5, -20, 55.5]
-    Salida:
-        TypeError
-
-    Entrada:
-        []
-    Salida:
-        ValueError
-
 Enunciat:
 Implementa la funció 'calculate_max_and_min' que rebi com a paràmetre una
 llista de números 'list_numbers', s'han de considerar els casos en què les
@@ -71,7 +39,6 @@ def calculate_max_and_min(list_numbers):
     pass
 
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
 # print(
