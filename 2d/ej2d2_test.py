c4e737829f11ec3b27f65ab601979f97f88760e3
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-04-17 04:27:53 +0200

    Afegim flake8 als tests

--- 2d/ej2d2_test.py
+++ 2d/ej2d2_test.py
@@ -1,5 +1,5 @@
 from ej2d2 import calculate_max_and_min
-
+from flake8.api import legacy as flake8
 
 def test_calculate_max_and_min(capfd):
     list_numbers = [10, 5.1, 0, -2, 31, 55, 70, -10, 200, -55.55]
@@ -9,3 +9,13 @@ def test_calculate_max_and_min(capfd):
     salida_stdout, salida_stderr = capfd.readouterr()
     assert salida_stdout.strip() != "", "calculate_max_and_min does not print anything"
     assert salida_stderr.strip() == "", "calculate_max_and_min prints an error message"
+
+
+def test_pep8_conformity():
+    style_guide = flake8.get_style_guide()
+    report = style_guide.check_files(["ej2d2.py"])
+    
+    assert report.get_statistics("") == [], (
+        "Your code does not comply with flake8. Please review your code"
+    )
+
--- 2d/ej2d3_test.py
+++ 2d/ej2d3_test.py
@@ -1,6 +1,6 @@
 from ej2d3 import division_list
 import pytest
-
+from flake8.api import legacy as flake8
 
 def test_division_list():
     # Lista con un string y un cero
@@ -16,3 +16,11 @@ def test_division_list():
             list_result ==
             [0.21126760563380284, 0.35211267605633806, 1.295774647887324, 0.0, 3.098591549295775]
     ), "division_list does not return the correct value for input [1.5, 2.5, 9.2, 0, 22] and 7.1. It should be [0.21126760563380284, 0.35211267605633806, 1.295774647887324, 0.0, 3.098591549295775]"
+
+def test_pep8_conformity():
+    style_guide = flake8.get_style_guide()
+    report = style_guide.check_files(["ej2d3.py"])
+    
+    assert report.get_statistics("") == [], (
+        "Your code does not comply with flake8. Please review your code"
+    )
