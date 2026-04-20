62032354d6b61216a793a732fc26db47c446dbe4
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-04-20 11:20:36 +0200

    Exercici acabat

--- 2d/ej2d1.py
+++ 2d/ej2d1.py
@@ -26,9 +26,15 @@ Exemple:
 
 
 def kg_to_lb(kg):
-    # Write here your code
-    pass
+    try:
+        if kg <= 0:
+            raise ValueError("Number must be greater than 0")
+        else:
+            lb = kg * 2.20462
+            return round(lb, 2)
+    except TypeError:
+        raise TypeError("Must be a number")
 
 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-#print(kg_to_lb(50))
+print(kg_to_lb(0))
